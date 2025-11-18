2e9322efca68d1f49143e0fa311fe4aa06642f0b
DigitRestore <162798020+DigitRestore@users.noreply.github.com>
2025-11-18 15:50:02 +0100

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,12 @@
 
+function loadComponent(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById(id).innerHTML = html;
+        });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadComponent("header", "components/header.html");
+});
